e964219036eb6fa419a8dd178bc03ed0be5de20f
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-17 01:24:34 +0530

    Added: Welcome Message

---
+++ src/main/java/com/bdlz/junituserregi/JunitUserRegistration.java
@@ -0,0 +1,8 @@
+package com.bdlz.junituserregi;
+
+public class JunitUserRegistration {
+    public static void main(String[] args) {
+        System.out.println("welcome to the junit user Registration program");
+    }
+
+}
---
+++ src/test/java/com/bdlz/junituserregi/JunitUserRegistrationTest.java
@@ -0,0 +1,8 @@
+package com.bdlz.junituserregi;
+
+import static org.junit.Assert.*;
+
+public class JunitUserRegistrationTest {
+
+
+}
