8881f58544244844f2c0235b8450e2fc3d3934ad
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-18 00:51:10 +0530

    Added: Conflict Resloved

--- src/main/java/com/bdlz/junituserregi/TestEmailIds.java
+++ src/main/java/com/bdlz/junituserregi/TestEmailIds.java
@@ -18,13 +18,13 @@ public class TestEmailIds {
 //            "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"
 
     public boolean testForValidEmails(String validEmails) {
-        regex = "^[a-zA-Z-9]+([._+-]*[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
+        regex = "^[a-zA-Z0-9]+([._+-]*[0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(validEmails);
         return matcher.matches();
     }
     public boolean testForNonValidEmails(String nonValidEmails) {
-        regex = "^[a-zA-Z-9]+([._+-][0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
+        regex = "^[a-zA-Z0-9]+([._+-][0-9A-Za-z]+)*@[a-zA-Z0-9]+.[a-zA-Z]{2,4}([.][a-z]{2,4})?$";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(nonValidEmails);
         return matcher.matches();
