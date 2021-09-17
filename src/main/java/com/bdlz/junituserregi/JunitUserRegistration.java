da7e32bcc823b7b64a4c6547e43d97cb6416cc12
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-09-17 15:30:01 +0530

    Added: Test for Firstname

--- src/main/java/com/bdlz/junituserregi/JunitUserRegistration.java
+++ src/main/java/com/bdlz/junituserregi/JunitUserRegistration.java
@@ -1,8 +1,14 @@
 package com.bdlz.junituserregi;
 
-public class JunitUserRegistration {
-    public static void main(String[] args) {
-        System.out.println("welcome to the junit user Registration program");
-    }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class JunitUserRegistration {
+    public static String regex;
+    public boolean firstName(String firstName) {
+        regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 }
--- src/test/java/com/bdlz/junituserregi/JunitUserRegistrationTest.java
+++ src/test/java/com/bdlz/junituserregi/JunitUserRegistrationTest.java
@@ -1,8 +1,22 @@
 package com.bdlz.junituserregi;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class JunitUserRegistrationTest {
+    JunitUserRegistration junitUserRegistration = new JunitUserRegistration();
 
+    @Test
+    public void givenFirstName_IsProper_ReturnTrue() {
+        boolean actualResult = junitUserRegistration.firstName("Ramakrishna");
+        Assert.assertEquals(true, actualResult);
+    }
 
+    @Test
+    public void givenFirstName_IsProper_ReturnFalse() {
+        boolean actualResult = junitUserRegistration.firstName("RamaKrishna");
+        Assert.assertEquals(false, actualResult);
+    }
 }
